a2d526265f4a9fc7a8eb302c5ad735df127315d0
Baxter Simwanza <baxter2809@gmail.com>
2021-02-18 11:38:56 +0100

    ResponseService class was created and injected into the controller. It contains the formatResponseItems function

--- src/main/java/com/gemography/challenge/controllers/GithubAPIController.java
+++ src/main/java/com/gemography/challenge/controllers/GithubAPIController.java
@@ -1,9 +1,7 @@
 package com.gemography.challenge.controllers;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.gemography.challenge.models.GitHubRepositoryModel;
-import com.gemography.challenge.models.ItemModel;
 import com.gemography.challenge.models.LanguagesResponseModel;
+import com.gemography.challenge.services.ResponseService;
 
 @RestController
 @RequestMapping("/top-github")
@@ -26,28 +24,11 @@ public class GithubAPIController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private ResponseService responseService;
+	
 	@Value("${GitHub-Repositories-API}")
 	private String gitHubReposApi;
-	
-	public Collection<LanguagesResponseModel> formatResponseItems(ArrayList<ItemModel> allInputItems) {
-
-		HashMap<String, LanguagesResponseModel> allResponseItems = new HashMap<String, LanguagesResponseModel>();
-
-		for(ItemModel item : allInputItems) {
-
-			LanguagesResponseModel currentResponseItem = allResponseItems.get(item.getLanguage());
-
-			if (currentResponseItem == null) {	
-				currentResponseItem = new LanguagesResponseModel(item.getLanguage(), 1);
-			} else {
-				currentResponseItem.setNumberOfUsers(currentResponseItem.getNumberOfUsers() + 1);
-			}
-
-			currentResponseItem.addNameToLanguageUsers(item.getName());
-			allResponseItems.put(item.getLanguage(), currentResponseItem);
-		}
-		return allResponseItems.values();
-	}
 	
 	public String customizeUrl(String url, Integer repositories) {
 		LocalDate date = LocalDate.now().minusDays(30);
@@ -69,6 +50,6 @@ public class GithubAPIController {
 
 		GitHubRepositoryModel gitHubRepositories = restTemplate.getForObject(url, GitHubRepositoryModel.class);
 		
-		return formatResponseItems(gitHubRepositories.getItems());
+		return responseService.formatResponseItems(gitHubRepositories.getItems());
 	}
 }
---
+++ src/main/java/com/gemography/challenge/services/ResponseService.java
@@ -0,0 +1,34 @@
+package com.gemography.challenge.services;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
+import org.springframework.stereotype.Service;
+
+import com.gemography.challenge.models.ItemModel;
+import com.gemography.challenge.models.LanguagesResponseModel;
+
+@Service
+public class ResponseService {
+
+	public Collection<LanguagesResponseModel> formatResponseItems(ArrayList<ItemModel> allInputItems) {
+
+		HashMap<String, LanguagesResponseModel> allResponseItems = new HashMap<String, LanguagesResponseModel>();
+
+		for(ItemModel item : allInputItems) {
+
+			LanguagesResponseModel currentResponseItem = allResponseItems.get(item.getLanguage());
+
+			if (currentResponseItem == null) {	
+				currentResponseItem = new LanguagesResponseModel(item.getLanguage(), 1);
+			} else {
+				currentResponseItem.setNumberOfUsers(currentResponseItem.getNumberOfUsers() + 1);
+			}
+
+			currentResponseItem.addNameToLanguageUsers(item.getName());
+			allResponseItems.put(item.getLanguage(), currentResponseItem);
+		}
+		return allResponseItems.values();
+	}
+}
