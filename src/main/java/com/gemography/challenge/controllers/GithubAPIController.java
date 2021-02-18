cb58d1035490a1aec5d662c2229c500e8b4bf407
Baxter Simwanza <baxter2809@gmail.com>
2021-02-18 09:12:18 +0100

    A customizeUrl function was added which enables the user to select the number of top repositories to return from the github API

--- src/main/java/com/gemography/challenge/controllers/GithubAPIController.java
+++ src/main/java/com/gemography/challenge/controllers/GithubAPIController.java
@@ -1,10 +1,14 @@
 package com.gemography.challenge.controllers;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,11 +24,27 @@ public class GithubAPIController {
 	
 	@Value("${GitHub-Repositories-API}")
 	private String gitHubReposApi;
+	
+	public String customizeUrl(String url, Integer repositories) {
+		LocalDate date = LocalDate.now().minusDays(30);
 
-	@GetMapping("/languages")
-	public GitHubRepositoryModel getLanguages() {
+		String finalUrl = url.replace("{date}", date.toString());
 
-		GitHubRepositoryModel gitHubRepositories = restTemplate.getForObject(gitHubReposApi.replace("{date}", "2021-01-18"), GitHubRepositoryModel.class);
+		if (repositories > 0) {
+			finalUrl = finalUrl.concat("&per_page="+repositories);
+		} else {
+			finalUrl = finalUrl.concat("&per_page="+100);
+		}
+
+		return finalUrl;
+	}
+	
+	@RequestMapping(value= {"/languages/repositories={repositories}"}, method = RequestMethod.GET)
+	public GitHubRepositoryModel getLanguages(@PathVariable(value = "repositories") Integer repositories) {
+
+		String url = customizeUrl(gitHubReposApi, repositories);
+
+		GitHubRepositoryModel gitHubRepositories = restTemplate.getForObject(url, GitHubRepositoryModel.class);
 		
 		return gitHubRepositories;
 	}
