a2440c30c1a409de4c64fd5652dfd9da3a9e9262
Baxter Simwanza <baxter2809@gmail.com>
2021-02-18 02:20:15 +0100

    First commit: The spring boot micro-service application initialized

---
+++ src/main/java/com/gemography/challenge/StarredGitRepositoriesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gemography.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarredGitRepositoriesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarredGitRepositoriesServiceApplication.class, args);
+	}
+
+}
