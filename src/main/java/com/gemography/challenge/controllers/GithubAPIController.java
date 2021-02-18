0cf7267434f77430034c4a7e2c1768f3dfc022a5
Baxter Simwanza <baxter2809@gmail.com>
2021-02-18 09:24:10 +0100

    formatResponseItems function added: It takes in the github API data, restructures it extracting the programming language-name, the number of users, and a list of top repository names who use it

--- src/main/java/com/gemography/challenge/controllers/GithubAPIController.java
+++ src/main/java/com/gemography/challenge/controllers/GithubAPIController.java
@@ -1,11 +1,13 @@
 package com.gemography.challenge.controllers;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.gemography.challenge.models.GitHubRepositoryModel;
+import com.gemography.challenge.models.ItemModel;
+import com.gemography.challenge.models.LanguagesResponseModel;
 
 @RestController
 @RequestMapping("/top-github")
@@ -24,6 +28,26 @@ public class GithubAPIController {
 	
 	@Value("${GitHub-Repositories-API}")
 	private String gitHubReposApi;
+	
+	public Collection<LanguagesResponseModel> formatResponseItems(ArrayList<ItemModel> allInputItems) {
+
+		HashMap<String, LanguagesResponseModel> allResponseItems = new HashMap<String, LanguagesResponseModel>();
+
+		for(ItemModel item : allInputItems) {
+
+			LanguagesResponseModel currentResponseItem = allResponseItems.get(item.getLanguage());
+
+			if (currentResponseItem == null) {	
+				currentResponseItem = new LanguagesResponseModel(item.getLanguage(), 1);
+			} else {
+				currentResponseItem.setNumberOfUsers(currentResponseItem.getNumberOfUsers() + 1);
+			}
+
+			currentResponseItem.addNameToLanguageUsers(item.getName());
+			allResponseItems.put(item.getLanguage(), currentResponseItem);
+		}
+		return allResponseItems.values();
+	}
 	
 	public String customizeUrl(String url, Integer repositories) {
 		LocalDate date = LocalDate.now().minusDays(30);
@@ -40,12 +64,11 @@ public class GithubAPIController {
 	}
 	
 	@RequestMapping(value= {"/languages/repositories={repositories}"}, method = RequestMethod.GET)
-	public GitHubRepositoryModel getLanguages(@PathVariable(value = "repositories") Integer repositories) {
-
+	public Collection<LanguagesResponseModel> getLanguages(@PathVariable(value = "repositories") Integer repositories) {
 		String url = customizeUrl(gitHubReposApi, repositories);
 
 		GitHubRepositoryModel gitHubRepositories = restTemplate.getForObject(url, GitHubRepositoryModel.class);
 		
-		return gitHubRepositories;
+		return formatResponseItems(gitHubRepositories.getItems());
 	}
 }
